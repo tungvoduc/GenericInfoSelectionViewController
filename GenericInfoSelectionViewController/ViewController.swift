ed5a068c1d9bf985b254063334872a76d6aac90f
Tung Vo <tung98.dn@gmail.com>
2018-04-13 19:47:15 +0300

    Initial Commit

---
+++ GenericInfoSelectionViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericInfoSelectionViewController
+//
+//  Created by Admin on 13/04/2018.
+//  Copyright © 2018 Tung Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
